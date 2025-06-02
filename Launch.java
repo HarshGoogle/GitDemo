59bd4814cb9c42622dffb36eb54450027cbf2e45
HarshGoogle <mr.harshvermateam@gmail.com>
2025-06-02 17:50:25 +0530

    Modifed after cloning by Dev1

--- Launch.java
+++ Launch.java
@@ -5,5 +5,11 @@ class Launch{
          System.out.println("First Git Demo");
 
   }
+  public static void fun(){
+     int a ;
+     int b;
+     int sum=a+b;
+     System.out.println("Sum = "+sum);
+  }
 
 }
